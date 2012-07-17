476cb4339e48e6028c100d5bac3669cda9878511
Kevin Hwang <k3vinhwang@gmail.com>
2012-07-17 13:45:10 -0700

    Add Game controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Dragonfruit::Application.routes.draw do
   root to: 'static_pages#home'
 
+  resources :game
+
   match "/help", to: 'static_pages#help'
 
   # The priority is based upon order of creation:
